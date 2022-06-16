c531463c25974e5a03b2c92524785dd24e396f54
igor.guariroba <igor.guariroba@jsl.com.br>
2022-06-16 17:10:11 -0300

    Tracker int

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="colunm is-one-quarter">
+    <BarraLateral/>
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioComponent/>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from "@/components/BarraLateral.vue";
+import FormularioComponent from "@/components/Formulario.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    FormularioComponent,
+    BarraLateral
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="box">
+    <div class="columns">
+
+      <div class="column is-8" role="form" aria-label="Fomulário para criação de uma nova tarefa">
+        <input type="text" class="input" placeholder="Qual terefa você deseja iniciar?">
+      </div>
+
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <select>
+            <strong>00:00:00</strong>
+          </select>
+          <button class="button">
+            <span class="icon">
+                <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button">
+            <span class="icon">
+                <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "FormularioComponent"
+})
+</script>
+
+<style scoped>
+
+</style>
